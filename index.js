1e7542ea5c6f59dbc05f929a3dfe9c28165d741a
Josep Servat <josep.servat@guidesmits.com>
2019-01-12 02:08:33 +0100

    Take Screenshot

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const puppeteer = require('puppeteer');
 
 const workFlow = async browser => {
   const page = await browser.newPage();
-  await page.setViewport({ width: 1200, heigth: 800 });
+  await page.setViewport({ width: 1200, height: 800 });
   await page.goto('http://www.zignaly.com');
+  await page.screenshot({ path: 'zignaly.png', fullPage: true });
   await browser.close();
 }
 
-puppeteer.launch()
+puppeteer.launch({ headless: false })
 .then(workFlow)
